6eea03844101fb9ac3fc11a966abe08f745f446e
Dieguin <diego.segura@geniac.com>
2017-03-17 11:47:49 +0100

    :art: [skills] improve skills gradient

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ export default Line.extend({
       chartData: {
         labels: ['Javascript', 'HTML', 'CSS', 'Angular.js 1 & 2', 'Vue.js', 'BackboneJS', 'Scrum', 'node.js', 'mongoDB'],
         datasets: [{
-          data: [80, 90, 92, 80, 75, 65, 87, 73, 76],
+          data: [80, 90, 92, 80, 75, 69, 87, 73, 76],
           borderColor: '#dc7067',
           borderWidth: 4,
           pointBackgroundColor: '#00dac0',
@@ -82,7 +82,7 @@ export default Line.extend({
     gradient.addColorStop(0.880, 'rgba(220, 112, 103, 1.000)');
     gradient.addColorStop(1.000, 'rgba(0, 0, 0, 0)');
 
-    const grd = ctx.createRadialGradient(100, 100, 0, 100, 0, 1200);
+    const grd = ctx.createRadialGradient(100, 0, 100, 600, 0, 700);
     grd.addColorStop(0, 'rgba(151,187,205,0.7)');
     grd.addColorStop(1, 'rgba(151,187,205,0)');
     this.setColor(gradient, grd);
